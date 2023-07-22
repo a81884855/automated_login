221f3d581a34395cd8ef31727c1e2d9a89469f29
Gary <garyguan0713@gmail.com>
2023-07-22 10:51:28 -0700

    refactor

--- eud_rocks.js
+++ eud_rocks.js
@@ -3,8 +3,6 @@ const Client = require('@infosimples/node_two_captcha');
 
 require('dotenv').config();
 
-console.log(process.env, 'process.env')
-
 // Declare your client
 const client = new Client(process.env.CAPTCHA_SOLVER_KEY, {
   timeout: 60000,
