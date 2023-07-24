83f0e6beaa67f395945bbc745a56f1c8cc399e58
Gary <garyguan0713@gmail.com>
2023-07-23 23:06:22 -0700

    revert original login file

--- eud_rocks.js
+++ eud_rocks.js
@@ -1,62 +1,16 @@
 const puppeteer = require('puppeteer');
-const Client = require('@infosimples/node_two_captcha');
-
 require('dotenv').config();
-
-// Declare your client
-const client = new Client(process.env.CAPTCHA_SOLVER_KEY, {
-  timeout: 60000,
-  polling: 5000,
-  throwErrors: false
-});
 
 (async () => {
   const browser = await puppeteer.launch(); //{ headless: false }
 
+  const cookie1 = { 'url': process.env.EUD_ROCKS_COOKIE_URL, 'name': process.env.EUD_ROCKS_COOKIE_1_NAME, 'value': process.env.EUD_ROCKS_COOKIE_1_VALUE }
+  const cookie2 = { 'url': process.env.EUD_ROCKS_COOKIE_URL, 'name': process.env.EUD_ROCKS_COOKIE_2_NAME, 'value': process.env.EUD_ROCKS_COOKIE_2_VALUE }
+
   const page = await browser.newPage();
+  await page.setCookie(cookie1, cookie2)
 
-  await page.goto('https://eud.rocks/login');
-
-  await page.waitForSelector('input#username');
-  await page.type('input#username', process.env.EUD_ROCKS_USERNAME);
-
-  await page.type('input#password', process.env.EUD_ROCKS_PASSWORD);
-
-  await page.waitForSelector('.captcha-clk2');
-
-  recaptchaBypass = async (execTimes = 0) => {
-    page.screenshot()
-    if (execTimes > 2) return console.log('retry 3 times');
-    const [imgResponse] = await Promise.all([
-      page.waitForResponse(imgResponse => imgResponse.url()),
-      page.click(execTimes === 0 ? '.captcha-clk2' : '.captcha-img'),
-    ]);
-
-    const buffer = await imgResponse.buffer();
-    console.log(buffer.toString('base64'), 'buffer')
-
-    await client.decode({
-      buffer,
-    }).then(async (response) => {
-      console.log(response.text);
-      await page.$eval('input#captcha', (el, value) => el.value = value, response.text);
-    });
-
-    await page.click('input[type=submit]')
-
-    try {
-      await page.waitForSelector('div.usertitle', { timeout: 5000 });
-      console.log('Login successed')
-    } catch {
-      console.log('Login failed');
-      return await recaptchaBypass(execTimes + 1)
-    }
-  }
-
-  await recaptchaBypass();
-
-  await page.waitForSelector('.close');
-  await page.click('.close');
+  await page.goto('https://eud.rocks/user-2');
 
   try {
     await page.$eval('a.usercheck.checkin', el => el.click());
